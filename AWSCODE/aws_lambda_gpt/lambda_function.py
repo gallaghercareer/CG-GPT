314a80e8261eb49b053176d38fbd7aec9e64b4d1
John Gallagher <gallaghercareer@gmail.com>
2023-12-23 17:34:52 -0500

    Update lambda_function.py

--- AWSCODE/aws_lambda_gpt/lambda_function.py
+++ AWSCODE/aws_lambda_gpt/lambda_function.py
@@ -4,6 +4,7 @@ import openai
 import os
 from datetime import datetime
 import asyncio
+import aiohttp
 
 # Configure logging to use /tmp directory in AWS Lambda
 log_file_path = '/tmp/openai_interaction_log.txt'
@@ -21,74 +22,76 @@ openai_api_key = "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"
 client = openai.OpenAI(api_key = openai_api_key)
 
 
+# Async function to create a thread
+async def create_thread():
+    url = "https://api.openai.com/v1/beta/threads"
+    headers = {"Authorization": f"Bearer {openai_api_key}"}
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, headers=headers) as response:
+            if response.status == 200:
+                data = await response.json()
+                thread_id = data['id']
+                write_log("create_thread() openai response object:" + str(data))
+                return thread_id
+            else:
+                error_message = await response.text()
+                write_log("Error in create_thread():" + error_message)
+                return None  # Handle error appropriately
 
-def create_thread():    
-    try:     
-        # Create a thread
-        thread = client.beta.threads.create()
-        thread_id = thread.id  
-        write_log("create_thread() openai response object:" + str(thread))     
-        return thread_id
-    except Exception as e:
-        write_log("Error in create_thread():" + str(e))
-        return f"An error occurred with create_thread() function: {e}"
+# Async function to send a message
+async def send_message(thread_id, user_message):
+    url = f"https://api.openai.com/v1/beta/threads/{thread_id}/messages"
+    headers = {"Authorization": f"Bearer {openai_api_key}"}
+    payload = {
+        "role": "user",
+        "content": user_message
+    }
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, json=payload, headers=headers) as response:
+            if response.status == 200:
+                data = await response.json()
+                write_log("send_message() openai response object:" + str(data))
+                return data
+            else:
+                error_message = await response.text()
+                write_log("Error in send_message():" + error_message)
+                return None  # Handle error appropriately
 
-def send_message(thread_id, user_message):
-    try:
-        # Send a message to the thread
-        message_response = client.beta.threads.messages.create(
-            thread_id=thread_id,
-            content = user_message,
-            role="user"
-        )
-        write_log("send_message() openai response object:" + str(message_response))   
-        return message_response
-    except Exception as e:
-        write_log("Error in create_thread():" +str(e))
-        return f"An error occurred while sending a message: {e}"
-    
-
-def run_assistant(thread_id, assistant_id):
-    try:
-        run = client.beta.threads.runs.create(
-            thread_id=thread_id,
-            assistant_id=assistant_id
-        )
-        write_log("run_assisstant() openai response object:" + str(run))  
-
-        messages = client.beta.threads.messages.list(
-            thread_id = thread_id
-        )
-
-        response = messages
-        write_log("run_assitsant() response object is as follows" + str(response))
-        return response
-    except Exception as e:
-        write_log("Error in create_thread():" + str(e))
-        return f"An error occurred while running assistant over thread session: {e}"
-
+# Async function to run assistant
+async def run_assistant(thread_id, assistant_id):
+    url = f"https://api.openai.com/v1/beta/threads/{thread_id}/runs"
+    headers = {"Authorization": f"Bearer {openai_api_key}"}
+    payload = {"assistant_id": assistant_id}
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, json=payload, headers=headers) as response:
+            if response.status == 200:
+                data = await response.json()
+                write_log("run_assistant() openai response object:" + str(data))
+                return data
+            else:
+                error_message = await response.text()
+                write_log("Error in run_assistant():" + error_message)
+                return None  # Handle error appropriately
 async def handler(event, context):
-
     try:
-       
-        #the Assistant ID
-        assitant_id = "asst_dRsEPaYci0NTsLzk5ot52hpf"
-
         body = json.loads(event['body'])
-
-        thread_id = body['thread_id']
-        user_message = body['user_message']
+        assistant_id = "asst_dRsEPaYci0NTsLzk5ot52hpf"  # Define outside for reuse
+        thread_id = body.get('thread_id')
+        user_message = body.get('user_message')
         
         if not thread_id:
-            # If thread_id is not provided, create a new thread
-            thread_id= create_thread()
-           
-        #add user's message to thread
-        send_message(thread_id, user_message)
+            thread_id = await create_thread()
+            if not thread_id:
+                raise Exception("Failed to create thread")
 
-        #assitant's response to the thread session
-        assistant_response = run_assistant(thread_id, assitant_id)  # Replace with your assistant ID
-        
+        message_response = await send_message(thread_id, user_message)
+        if message_response is None:
+            raise Exception("Failed to send message")
+
+        assistant_response = await run_assistant(thread_id, assistant_id)
+        if assistant_response is None:
+            raise Exception("Failed to run assistant")
+
         # Read the log file and include its contents in the response
         log_contents = ""
         if os.path.exists(log_file_path):
@@ -97,25 +100,24 @@ async def handler(event, context):
 
         response_body = {
             "assistant_response": assistant_response,
-            "thread_id" : thread_id,
-            "log_contents": log_contents           
+            "thread_id": thread_id,
+            "log_contents": log_contents
         }
-        
-        # Return the line count as the response
+
         return {
             "isBase64Encoded": False,
             "statusCode": 200,
-            "headers": { "Content-Type": "application/json" },
-            "body": json.dumps(response_body)           
+            "headers": {"Content-Type": "application/json"},
+            "body": json.dumps(response_body)
         }
-        
-    except Exception as e:
 
+    except Exception as e:
         write_log("Main Handler Exception:" + str(e))
         log_contents = ""
         if os.path.exists(log_file_path):
             with open(log_file_path, 'r') as log_file:
                 log_contents = log_file.read()
+
         # Correctly format the error response according to Lambda proxy integration requirements
         error_response = {
             "errorMessage": str(e),
