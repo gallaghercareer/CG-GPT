5b00deda7ee0c7097b16846dcacdce9dd4aefa97
John Gallagher <gallaghercareer@gmail.com>
2023-12-26 19:54:19 -0500

    cleaned up lambda

--- AWSCODE/aws_lambda_gpt/lambda_function.py
+++ AWSCODE/aws_lambda_gpt/lambda_function.py
@@ -19,12 +19,8 @@ def get_parameter(param_name):
 # Get OpenAI API key  and Assistant ID from Parameter Store
 openai_api_key = get_parameter("openai_api_key")
 assistant_id = get_parameter("assistant_id_achievementaward")
-print(assistant_id)
-print(openai_api_key)
 
 #Open AI Client Object
-#we actually need this from the STORE system parameter
-#openai_api_key = "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"
 client = openai.OpenAI(api_key = openai_api_key)
 
 # Configure logging to use /tmp directory in AWS Lambda
@@ -75,14 +71,9 @@ async def run_assistant(thread_id, assistant_id):
             assistant_id=assistant_id
         )
         run_id = run.id  
-        print("runid:"+run_id) 
-        print("assistantid:"+assistant_id)
-        print("threadid:"+thread_id)
 
-     
         while run.status != 'completed':
             if run.status == 'required_action':
-
                 raise Exception("Run failed: " + str(run.last_error) + "\n " + str(run.required_action))
             if run.status =='failed':
                 raise Exception("Run failed: " + str(run.last_error) + "\n " + str(run.required_action))                    
@@ -108,20 +99,12 @@ async def run_assistant(thread_id, assistant_id):
 async def async_handler(event, context, thread_id):    
     try:       
            
-        #body = json.loads(event['body'])
-        
-        #user_message = body['user_message']
-          
         #assitant's response to the thread session
         message = await run_assistant(thread_id, assistant_id)
 
         print(message)
-        # Assuming 'response' is the list of ThreadMessage objects you provided
         first_ai_message = message[0].content[0].text.value
-      
-        # Read the log file and include its contents in the response
-        #log_contents = await readlog()
-          
+
         response_body = {
             "assistant_response": first_ai_message,
             "thread_id" : thread_id,
@@ -138,11 +121,9 @@ async def async_handler(event, context, thread_id):
         
     except Exception as e:
         await write_log("Main Handler Exception:" + str(e))
-        #log_contents = await readlog()
         # Correctly format the error response according to Lambda proxy integration requirements
         error_response = {
             "errorMessage": str(e),
-            #"log_contents": log_contents
         }
         
         return {
